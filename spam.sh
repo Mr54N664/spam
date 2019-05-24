793dca9f232abdee2a7649424ef8ed41c61852da
Mr54N664 <50640066+Mr54N664@users.noreply.github.com>
2019-05-24 18:47:44 +0700

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,93 @@
+#/bin/sh
+
+Cy='\033[34;1m' #biru            
+i='\033[32;1m' #ijo    
+pur='\033[35;1m' #purple
+e='\033[36;1m' #cyan 
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+clear
+figlet J A N C O K  K A U | lolcat
+echo  "|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|" | lolcat
+echo  "|â€¢|               ""By  : Mr.54N664""                 |â€¢|" | lolcat
+echo  "|â€¢|         ""Wa  : 0888936xxxx privasi:v""           |â€¢|" | lolcat
+echo  "|â€¢|     ""Fb  : https://m.facebook.com/punk.3999""    |â€¢|" | lolcat
+echo  "|â€¢|     ""github : https://github.com/******:v""      |â€¢|" | lolcat
+echo  "|â€¢|        ""YT : RezkyId23 subscribe sem***""        |â€¢|" | lolcat
+echo  "|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|â€¢|" | lolcat
+sleep 1
+echo "Pilih Nomornya :" | lolcat
+echo "[1] S P A M C A L L  (SpamTelepon)" | lolcat
+echo "[2] S P A M S M S (SpamSms)" | lolcat
+echo "[3] S P A M G M A I L" | lolcat
+echo "[4] S P A M W A T S A P P (W O R K I N 30%)" | lolcat
+echo "[5] E X I T" | lolcat
+echo
+read -p"Pilih Angka Jancox==> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+echo " Install SpamCall.. " | lolcat
+sleep 1
+pkg install php -y
+git clone https://github.com/ClayHackerTeam/311
+cd 311
+php SpamCall.php
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"| lolcat
+echo " Install SpamSms.. " | lolcat
+pkg  install php -y
+git clone https://github.com/alfianokt/caping-spam
+cd caping-spam
+php run.php
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+echo " install SpamGmail.." | lolcat
+pkg install php -y
+git clone https://github.com/GebangKiidiw/mail-spammer
+cd mail-spammer
+php mail.php
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+echo " Install SpamWhatsapp.." | lolcat
+pkg install php -y
+git clone https://github.com/siputra12/prank
+cd prank
+php wa.php
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "E X I T"| lolcat
+sleep 2
+echo $pur"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $pur"WhatsApp :"$pur" 08889362204"
+sleep 2
+echo $i">> Thanks Yang Sudah Support Saya <<"
+sleep 2
+echo $pur"bangkitlah dari kesalahan tapi jika anda udah tau itu salah tapi anda tetap melakukan nya"
+sleep 2
+echo $i"berarti anda goblok,tolol,bego makanya jangan terlalu banyak coli coeg:v"
+sleep 2
+exit
+fi
